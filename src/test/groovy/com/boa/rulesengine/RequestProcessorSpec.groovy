82926a2b070c1b9d85a5a5652a015a4b4dc211a7
Vijay Inani <vijayinani@gmail.com>
2018-09-15 17:11:59 +0530

    Added Spock dependency and test case

---
+++ src/test/groovy/com/boa/rulesengine/RequestProcessorSpec.groovy
@@ -0,0 +1,50 @@
+package com.boa.rulesengine
+
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class RequestProcessorSpec extends Specification {
+
+    @Shared
+    private Map<String, Set<Node>> rulesCache
+
+
+    def setupSpec() {
+        rulesCache = RequestProcessor.loadRules()
+    }
+
+    @Unroll
+    void "test the rule engine #input #output"() {
+        expect:
+        output == RequestProcessor.getResult(input, rulesCache)
+
+        where:
+        input                     | output
+        "Tax,NSE,2001,*,AA,*"     | "100"
+        "Tax,*,3001,IN,AB,CD"     | "90"
+        "Tax,*,*,*,*,*"           | "50"
+        "Instrument,NSDQ,1001,US" | "10"
+        "Instrument,NYSE,1001,US" | "20"
+        "Instrument,BSE,1001,*"   | "10"
+        "Instrument,BSE,*,*"      | "5"
+        "Instrument,*,2001,IN"    | "2"
+        "Instrument,*,*,US"       | "6"
+        "Instrument,*,*,*"        | "20"
+        "Instrument,NSE,2001,*"   | "20"
+        "Fees,NSE,2001,*"         | null
+        "Fees,NSE,*,*"            | null
+    }
+
+//    @Unroll
+//    public void "test the rule engine for exception #input"() {
+//        when:
+//        RequestProcessor.getResult(input, rulesCache)
+//
+//        then:
+//        thrown(Exception)
+//
+//        where:
+//        input << ["vijay"]
+//    }
+}
